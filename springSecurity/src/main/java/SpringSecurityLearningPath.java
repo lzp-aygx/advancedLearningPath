be057171c8e2ad2de4cccde47e8bb39020d36a8a
Aygx <277980904@qq.com>
2017-09-19 22:57:14 +0800

    update
    Signed-off-by: Aygx <277980904@qq.com>

--- springSecurity/src/main/java/SpringSecurityLearningPath.java
+++ springSecurity/src/main/java/SpringSecurityLearningPath.java
@@ -1,8 +1,10 @@
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * Created by lizhupeng on 2017/9/19.
  */
+@SpringBootApplication
 public class SpringSecurityLearningPath {
 
     public static void main(String[] args) {
--- springSecurity/src/main/java/config/SpringSecurityConfig.java
+++ springSecurity/src/main/java/config/SpringSecurityConfig.java
@@ -1,11 +1,12 @@
 package config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 import javax.annotation.Resource;
 
@@ -14,41 +15,31 @@ import javax.annotation.Resource;
  */
 @Configuration
 @EnableWebSecurity
-public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+public class SpringSecurityConfig {
+
+//    @Resource
+//    private AccountAuthenticationProvider accountAuthenticationProvider;
 
     @Resource
-    AccountAuthenticationProvider accountAuthenticationProvider;
-
-
-    @Resource
-    public void configureGlobal(AuthenticationManagerBuilder auth){
-        auth.authenticationProvider(accountAuthenticationProvider);
-    }
-
-
-    protected void configure(HttpSecurity http) throws Exception {
-        //屏蔽防跨域请求.
-        http.csrf().disable();
-        http.sessionManagement().disable();
-
-//        http.authorizeRequests()
-//                .antMatchers("/", "/login").permitAll()//配置根路径和/login路径为不拦截路径
-//                .antMatchers("/admin/**").hasRole("ADMIN")
-//                .anyRequest().authenticated()
-//                .and().formLogin().loginPage("/login").failureUrl("/401.html").defaultSuccessUrl("/index").permitAll()//配置登录页面
-//                .and().logout().permitAll();//配置注销登出页面
-    }
-
-    //在内存中分别配置两个用户,角色是USER
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
-                .withUser("lzp").password("lzp").roles("USER")
-                .and()
-                .withUser("lizhupeng").password("lizhupeng").roles("USER");
+                .withUser("admin")
+                .password("password")
+                .roles("ADMIN");
+//        auth.authenticationProvider(accountAuthenticationProvider);
     }
 
-    //忽略静态资源的拦截
-    public void configure(WebSecurity web) throws Exception{
-        web.ignoring().antMatchers("/resources/static/**");
+    @Configuration
+    @Order(1)
+    public static class ApiWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter{
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/api/**").authorizeRequests().anyRequest().hasRole("USER")
+                    .and()
+                    .httpBasic()
+                    .and()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        }
     }
 }
